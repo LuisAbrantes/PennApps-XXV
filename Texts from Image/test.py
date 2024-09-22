1dcdc18ce1a41e768e72c8e56c8a64f999019d2c
Luis <luis.hsa@gmail.com>
2024-09-21 21:25:00 -0400

    Pyssetact + Virtual Env. Added

--- Texts from Image/test.py
+++ Texts from Image/test.py
@@ -1,3 +1,8 @@
-from ocrmac import ocrmac
-annotations = ocrmac.OCR('test.png').recognize()
-print(annotations)
+from PIL import Image
+import pytesseract
+
+
+img = Image.open('test.png')
+text = pytesseract.image_to_string(img)
+
+print(text)
